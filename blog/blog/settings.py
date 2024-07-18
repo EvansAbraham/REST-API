6846aa0c92238a11950ea8b2bd5a60dd37b3946f
Evans Abraham <65326443+EvansAbraham@users.noreply.github.com>
2024-07-19 01:20:55 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-lukklk3hh35i$$)ocslg_!ctjih(0f5g!z--t$j7)_jd!82u87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'rest-api-ohtp.onrender.com',
+]
 
 
 # Application definition
